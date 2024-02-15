caae9ae4af1320fcd326fef44eb154f5673b9d81
Micah Villaruz <micahmvillaruz@gmail.com>
2024-02-15 22:56:47 +0800

    Add Factory and Seeders

---
+++ database/factories/ItemFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Item;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Item>
+ */
+class ItemFactory extends Factory
+{
+    protected $model = Item::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->word(),
+            'description' => fake()->text(),
+            'price' => fake()->randomFloat(2, 10, 500),
+            'quantity' => fake()->numberBetween(1, 100)
+        ];
+    }
+}
